a93cb964c6096de334e39a3399e564d3ce33e2b1
Jeremy <jthughes@users.noreply.github.com>
2025-12-15 10:46:47 +1030

    Validated and filter, with rework of output packages

--- src/api/chirps.ts
+++ src/api/chirps.ts
@@ -1,31 +1,31 @@
 import type { Request, Response, NextFunction } from "express";
-import { parse } from "path/win32";
+
+import { respondWithError, respondWithJSON } from "./json.js";
 
 export async function handlerValidateChirp(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-  let body = "";
+  type parameters = {
+    body: string;
+  };
 
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
+  const params: parameters = req.body;
 
-  req.on("end", () => {
-    try {
-      type chirp = {
-        body: string;
-      };
-      const parseBody: chirp = JSON.parse(body);
-      if (parseBody.body.length > 140) {
-        throw new Error("Chirp is too long");
-      }
-      res.status(200).send({ valid: true });
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(400).send({ error: error.message });
-      }
+  const maxChirpLength = 140;
+  if (params.body.length > maxChirpLength) {
+    respondWithError(res, 400, "Chirp is too long");
+    return;
+  }
+  const filter = ["kerfuffle", "sharbert", "fornax"];
+  const input = params.body.split(" ");
+
+  for (let i = 0; i < input.length; i++) {
+    if (filter.includes(input[i].toLowerCase())) {
+      input[i] = "****";
     }
-  });
+  }
+  const output = input.join(" ");
+  respondWithJSON(res, 200, { cleanedBody: output });
 }
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogging);
+app.use(express.json());
 
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
